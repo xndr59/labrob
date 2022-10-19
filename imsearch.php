b9c78556e325d776f66546f8278bbbfa8554d812
xndr59 <116182038+xndr59@users.noreply.github.com>
2022-10-19 17:14:26 +0300

    Add files via upload

---
+++ imsearch.php
@@ -0,0 +1,127 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="ua-UA" dir="ltr">
+	<head>
+		<title>Поиск - E-31</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="Бараненко Олександр" />
+		<meta name="generator" content="Incomedia WebSite X5 Professional 13.0.2.19 - www.websitex5.com" />
+		<meta property="og:locale" content="ua" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://www.group31/imsearch.php" />
+		<meta property="og:title" content="Поиск" />
+		<meta property="og:site_name" content="E-31" />
+		<meta property="og:image" content="http://www.group31/favImage.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="4080">
+		<meta property="og:image:height" content="1840">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?13-0-2-19" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?13-0-2-19" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?13-0-2-19" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?13-0-2-19" media="screen" />
+		<link rel="stylesheet" type="text/css" href="style/menu.css?13-0-2-19" media="screen" />
+		<link rel="stylesheet" type="text/css" href="pcss/imsearch.css?13-0-2-19-638017962361355770" media="screen" />
+		<script type="text/javascript" src="res/jquery.js?13-0-2-19"></script>
+		<script type="text/javascript" src="res/x5engine.js?13-0-2-19" data-files-version="13-0-2-19"></script>
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('Ваш браузер не підтримує функції, необхідні для візуалізації цього сайту.','Ваш браузер не підтримує функції, необхідні для візуалізації цього сайту.','[1]Оновіть Ваш браузер[/1] або [2]продовжите процедуру [/2].','http://outdatedbrowser.com/'); };
+		</script>
+		<link rel="icon" href="favicon.png?13-0-2-19-638017962361335776" type="image/png" />
+	</head>
+	<body>
+		<div id="imHeaderBg"></div>
+		<div id="imFooterBg"></div>
+		<div id="imPage">
+			<div id="imHeader">
+				<h1 class="imHidden">Поиск - E-31</h1>
+				<div id="imHeaderObjects"><div id="imHeader_imTextObject_01_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_1" class="" > <div id="imHeader_imCellStyleGraphics_1"></div><div id="imHeader_imCellStyle_1" ><div id="imHeader_imTextObject_01">
+	<div class="text-tab-content"  id="imHeader_imTextObject_01_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: center;"><span class="fs18"><i>Lorem ipsum dolor sit amet.</i></span></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imHeader_imObjectImage_02_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_2" class="" > <div id="imHeader_imCellStyleGraphics_2"></div><div id="imHeader_imCellStyle_2" ><img id="imHeader_imObjectImage_02" src="images/GT_logo_4.png" title="" alt="" /></div></div></div><div id="imHeader_imObjectImage_03_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_3" class="" > <div id="imHeader_imCellStyleGraphics_3"></div><div id="imHeader_imCellStyle_3" ><img id="imHeader_imObjectImage_03" src="images/Black_no.circle_facebook.png" title="" alt="" /></div></div></div><div id="imHeader_imObjectImage_06_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_6" class="" > <div id="imHeader_imCellStyleGraphics_6"></div><div id="imHeader_imCellStyle_6" ><img id="imHeader_imObjectImage_06" src="images/Black_no.circle_twitter.png" title="" alt="" /></div></div></div><div id="imHeader_imObjectImage_07_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_7" class="" > <div id="imHeader_imCellStyleGraphics_7"></div><div id="imHeader_imCellStyle_7" ><img id="imHeader_imObjectImage_07" src="images/black_no.circle_youtube.png" title="" alt="" /></div></div></div></div>
+			</div>
+			<a class="imHidden" href="#imGoToCont" title="Заголовок головного меню">Перейти до контакту</a>
+			<a id="imGoToMenu"></a><p class="imHidden">Головне меню:</p>
+			<div id="imMnMnContainer">
+				<div id="imMnMnGraphics"></div>
+				<div id="imMnMn" class="auto main-menu">
+					<div class="hamburger-site-background menu-mobile-hidden"></div><div class="hamburger-button"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container"><div class="hamburger-menu-background menu-mobile-hidden"><div class="hamburger-menu-close-button"><span>&times;</span></div></div></div>
+				<ul class="auto menu-mobile-hidden">
+					<li id="imMnMnNode0" class=" imPage">
+						<a href="index.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Главная</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode3" class=" imPage">
+						<a href="---------1.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Страница 1</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode4" class=" imPage">
+						<a href="---------2.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Страница 2</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode5" class=" imPage">
+						<a href="---------3.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Страница 3</span></span>
+							</span>
+						</a>
+					</li></ul>
+				</div>
+			</div>
+			<div id="imContentContainer">
+				<div id="imContentGraphics"></div>
+				<div id="imContent">
+					<a id="imGoToCont"></a>
+				<div id="imSearchPage">
+				<h2 id="imPgTitle">Результати пошуку</h2>
+				<?php
+				$search = new imSearch();
+				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+				<div class="searchPageContainer">
+				<?php echo $search->search($keys, $page, $type); ?>
+				</div>
+				</div>
+				
+					<div class="imClear"></div>
+				</div>
+			</div>
+			<div id="imFooter">
+				<div id="imFooterObjects"><div id="imFooter_imTextObject_01_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_1" class="" > <div id="imFooter_imCellStyleGraphics_1"></div><div id="imFooter_imCellStyle_1" ><div id="imFooter_imTextObject_01">
+	<div class="text-tab-content"  id="imFooter_imTextObject_01_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: center;"><span class="fs8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae congue augue, nec volutpat risus. Quisque luctus, lacus eu molestie porta, </span></div><div style="text-align: center;"><span class="fs8">dolor augue ullamcorper augue, sed iaculis metu.</span><span class="fs11 ff1"> </span></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imFooter_imObjectImage_02_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_2" class="" > <div id="imFooter_imCellStyleGraphics_2"></div><div id="imFooter_imCellStyle_2" ><img id="imFooter_imObjectImage_02" src="images/Grey_no.circle__cart.png" title="" alt="" /></div></div></div><div id="imFooter_imObjectSearch_03_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_3" class="" > <div id="imFooter_imCellStyleGraphics_3"></div><div id="imFooter_imCellStyle_3" ><div id="imFooter_imObjectSearch_03"><form id="imFooter_imObjectSearch_03_form" action="imsearch.php" method="get"><fieldset><input type="text" id="imFooter_imObjectSearch_03_field" name="search" value="" /><button id="imFooter_imObjectSearch_03_button">Cerca</button></fieldset></form><script>$('#imFooter_imObjectSearch_03_button').click(function() { $('#imFooter_imObjectSearch_03_form').submit(); return false; });</script></div></div></div></div><div id="imFooter_imTextObject_04_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_4" class="" > <div id="imFooter_imCellStyleGraphics_4"></div><div id="imFooter_imCellStyle_4" ><div id="imFooter_imTextObject_04">
+	<div class="text-tab-content"  id="imFooter_imTextObject_04_tab0" style="">
+		<div class="text-inner">
+			<b><span class="cf1"><i class="fs14">Lorem Ipsum!</i></span></b>
+		</div>
+	</div>
+
+</div>
+</div></div></div></div>
+			</div>
+		</div>
+		<span class="imHidden"><a href="#imGoToCont" title="Прочитати цю сторінку знову">Назад до змісту</a> | <a href="#imGoToMenu" title="Прочитати цей час знову">Назад до головного меню</a></span>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Щоб використовувати цей сайт, необхідно включити JavaScript</div></noscript>
+	</body>
+</html>
